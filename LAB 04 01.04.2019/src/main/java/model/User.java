51e8153f2ee0f3a36f8faedd0a0b74fae5028017
Temus <temus@wir.pl>
2019-04-14 11:44:56 +0200

    v0.0.5

--- LAB 04 01.04.2019/src/main/java/model/User.java
+++ LAB 04 01.04.2019/src/main/java/model/User.java
@@ -2,18 +2,70 @@ package model;
 
 public class User {
 
-    private String login, password, name, surname, email;
+
+    Long id;
+    private String name, surname, login, password, email;
 
     private enum Permissions {
         USER, ADMINISTRATOR, NULL;
     }
 
-    public User(String login, String password, String name, String surname, String email) {
-        this.login = login;
-        this.password = password;
+    public User(Long id, String name, String surname, String login, String password, String email) {
+        this.id = id;
         this.name = name;
         this.surname = surname;
+        this.login = login;
+        this.password = password;
         this.email = email;
         Permissions permissions = Permissions.USER;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }

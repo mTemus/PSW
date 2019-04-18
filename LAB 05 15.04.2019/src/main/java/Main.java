e37ee224edfad82f3b84d0430a98d208134d018e
Temus <temus@wir.pl>
2019-04-18 13:13:40 +0200

    V0.1.1

--- LAB 05 15.04.2019/src/main/java/Main.java
+++ LAB 05 15.04.2019/src/main/java/Main.java
@@ -8,8 +8,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../resources/eventView.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("loginView.fxml"));
+        primaryStage.setTitle("Login View.");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
--- LAB 05 15.04.2019/src/main/java/model/user.java
+++ LAB 05 15.04.2019/src/main/java/model/user.java
@@ -1,4 +1,30 @@
 package model;
 
 public class user {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+    private String email;
+    private String date;
+
+    private enum Permissions {
+        USER, ADMINISTRATOR
+    }
+
+    private enum FoodPreferences {
+        VEGAN, NO_GLUTEN, NO_PREFFERENCES
+    }
+
+    private enum ParticipationType {
+        LISTENER, AUTHOR, SPONSOR, ORGANIZER
+    }
+
+    private Permissions permissions = null;
+    private FoodPreferences foodPerfferences = null;
+    private ParticipationType participationType = null;
+
+
 }

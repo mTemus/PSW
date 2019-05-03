58ff855d8655a8939b2e4bc0c33da2b6247d40cf
Temus <temus@wir.pl>
2019-05-03 15:05:15 +0200

    v0.4.0

--- LAB 06 29.04.2019/src/main/java/hibernateModel/User.java
+++ LAB 06 29.04.2019/src/main/java/hibernateModel/User.java
@@ -1,6 +1,5 @@
 package hibernateModel;
 
-
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -68,5 +67,86 @@ public class User {
         this.permissions = model.User.Permissions.USER.toString();
         this.foodPreferences = null;
         this.participationType = null;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
+
+    public model.User.FoodPreferences getFoodPreferences() {
+        return foodPreferences;
+    }
+
+    public void setFoodPreferences(model.User.FoodPreferences foodPreferences) {
+        this.foodPreferences = foodPreferences;
+    }
+
+    public model.User.ParticipationType getParticipationType() {
+        return participationType;
+    }
+
+    public void setParticipationType(model.User.ParticipationType participationType) {
+        this.participationType = participationType;
     }
 }
---
+++ LAB 06 29.04.2019/src/main/java/hibernateOperations/EntityOperations.java
@@ -0,0 +1,40 @@
+package hibernateOperations;
+
+import hibernateModel.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityOperations {
+
+
+    public EntityManager myEntityManager() {
+        EntityManagerFactory entityFactory = Persistence.createEntityManagerFactory("org.hibernate.event_server.jpa");
+        EntityManager entityManager = entityFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+//        entityManager.close();
+//        entityFactory.close();
+
+
+//        entityManager.persist(object); <-- dodawanie do bazy danych
+        // entityManager.getTransaction().commit(); <-- potwierdzenie operacji
+        //entityManager.merge(object) <-- update
+        //entityManager.find(Object.class, argument);
+        //entityManager.delete(Object);
+
+        String queryToExecute = "";
+        Query query = entityManager.createQuery(queryToExecute);
+
+
+        User user = (User) query.getSingleResult();
+        List<User> userList = query.getResultList();
+
+        return entityManager;
+    }
+}

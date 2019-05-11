677c5e66d8d941d7492786ded5e1571687863979
Temus <temus@wir.pl>
2019-05-11 17:27:19 +0200

    v0.5

--- LAB 06 29.04.2019/src/main/java/hibernateOperations/DatabaseLoginOperations.java
+++ LAB 06 29.04.2019/src/main/java/hibernateOperations/DatabaseLoginOperations.java
@@ -2,23 +2,18 @@ package hibernateOperations;
 
 import hibernateModel.User;
 
-import javax.jws.soap.SOAPBinding;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
 public class DatabaseLoginOperations {
-    private User tmp_user = null;
     private EntityManagerOperations EMO = new EntityManagerOperations();
 
     public User searchForExistingUser(String userLogin, String userPassword) {
         User existingUser = null;
-
-        EntityManager entityManager = EMO.getEntityManagerFactory().createEntityManager();
-
         String query = "SELECT u FROM User u WHERE u.login = :uLogin AND u.password = :uPassword";
 
-        TypedQuery<User> typedQuery = entityManager.createQuery(query, User.class);
+        TypedQuery<User> typedQuery = EMO.getEntityManager().createQuery(query, User.class);
         typedQuery.setParameter("uLogin", userLogin);
         typedQuery.setParameter("uPassword", userPassword);
 
@@ -29,10 +24,15 @@ public class DatabaseLoginOperations {
         } catch (NoResultException nre) {
             nre.printStackTrace();
         } finally {
-            EMO.getEntityManagerFactory().close();
+            EMO.closeConnection();
         }
-
         return existingUser;
+    }
+
+    public void addUserToDatabase(User userToAdd) {
+        EntityManager entityManager = EMO.getEntityManagerFactory().createEntityManager();
+        entityManager.persist(userToAdd);
+        EMO.closeConnection();
     }
 
 }
--- LAB 06 29.04.2019/src/main/java/hibernateOperations/EntityManagerOperations.java
+++ LAB 06 29.04.2019/src/main/java/hibernateOperations/EntityManagerOperations.java
@@ -1,12 +1,22 @@
 package hibernateOperations;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class EntityManagerOperations {
     private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Event-Server");
+    private static EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+
+    public void closeConnection(){
+        entityManagerFactory.close();
+    }
 
     public EntityManagerFactory getEntityManagerFactory() {
         return entityManagerFactory;
+    }
+    public EntityManager getEntityManager() {
+        return entityManager;
     }
 }
